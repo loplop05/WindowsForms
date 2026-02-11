9c7b4a78c11da297901da93fc0fd44129cbe01f3
loplop05 <ammaralharoon@hotmail.com>
2026-02-11 22:04:30 +0300

    Progress Bar

--- testForms/frmProgressBar.Designer.cs
+++ testForms/frmProgressBar.Designer.cs
@@ -29,11 +29,78 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.progressBar1.Location = new System.Drawing.Point(382, 213);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(360, 47);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(349, 289);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(169, 48);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Increase Progress Bar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(608, 289);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(169, 48);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Reset Progress Bar";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(372, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(385, 135);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // frmProgressBar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1200, 627);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "frmProgressBar";
             this.Text = "frmProgressBar";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
--- testForms/frmProgressBar.cs
+++ testForms/frmProgressBar.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +16,41 @@ namespace testForms
         public frmProgressBar()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            progressBar1.Value = 0;
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = 100;
+
+            for(int i = 0; i<=10;i++)
+            {
+                if(progressBar1.Value < progressBar1.Maximum)
+                {
+                    Thread.Sleep(500);
+                    progressBar1.Value += 10;
+                    label1.Text = (((float)progressBar1.Value / progressBar1.Maximum) * 100) + "%";
+
+                    progressBar1.Refresh();
+                    label1.Refresh();
+                }
+                else
+                {
+                    button1.Enabled = false;
+
+                }
+            }
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
